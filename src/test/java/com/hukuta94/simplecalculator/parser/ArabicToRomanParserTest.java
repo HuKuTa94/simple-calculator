9010c8b800cc9dd53e1ad3a35c2da0135060e237
aveJava <javatalk@mail.ru>
2020-12-06 21:47:08 +0300

    added unit-tests for classes ArabicToRomanParser and RomanToArabicParser

--- src/test/java/com/hukuta94/simplecalculator/parser/ArabicToRomanParserTest.java
+++ src/test/java/com/hukuta94/simplecalculator/parser/ArabicToRomanParserTest.java
@@ -1,4 +1,17 @@
 package com.hukuta94.simplecalculator.parser;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 public class ArabicToRomanParserTest {
+    @Test
+    @DisplayName("Parser ArabicToRoman")
+    public void testParse(){
+        for (int i=1; i<4000; i++) {
+            String roman = ArabicToRomanParser.parse(i);
+            int arabic = RomanToArabicParser.parse(roman);
+            Assertions.assertEquals(i, arabic);
+        }
+    }
 }
