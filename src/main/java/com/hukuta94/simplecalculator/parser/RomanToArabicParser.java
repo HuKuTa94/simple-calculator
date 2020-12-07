9fee353c0b7f45faafbba6e29a25c9fd4eecd4b9
aveJava <javatalk@mail.ru>
2020-12-07 18:47:54 +0300

    rewritten class RomanToArabicParser

--- src/main/java/com/hukuta94/simplecalculator/parser/RomanToArabicParser.java
+++ src/main/java/com/hukuta94/simplecalculator/parser/RomanToArabicParser.java
@@ -1,5 +1,11 @@
 package com.hukuta94.simplecalculator.parser;
 
+import com.hukuta94.simplecalculator.model.RomanNumeral;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
 public class RomanToArabicParser {
     // римское число, которое надо распарсить
     private static StringBuilder line;
@@ -10,76 +16,29 @@ public class RomanToArabicParser {
         line = new StringBuilder(romanNumber);
         int arabicNumber = 0;
 
-        // парсинг тысяч, сотен, десятков и единиц
-        int thousand = auxiliaryParse(1000, 'M', 'K', 'K');
-        int hundreds = auxiliaryParse(100, 'C', 'D', 'M');
-        int tens = auxiliaryParse(10, 'X', 'L', 'C');
-        int units = auxiliaryParse(1, 'I', 'V', 'X');
+        ArrayList<RomanNumeral> list = new ArrayList<>(Arrays.asList(RomanNumeral.values()));
+        Collections.reverse(list);
+        System.out.println(list);
 
-        // проверка на некоректность полученных результатов
-        if (thousand > 3000) return -1;
-        if (thousand == tens && hundreds == tens && units == tens && tens == -1) return -1;
+        for (RomanNumeral item : list) {
+            if (checkAndPrun(item.getRoman())) arabicNumber += item.getArabic();
+        }
 
-        // собираем промежуточные данные в итоговое значение
-        if (thousand != -1) arabicNumber += thousand;
-        if (hundreds != -1) arabicNumber += hundreds;
-        if (tens != -1) arabicNumber += tens;
-        if (units != -1) arabicNumber += units;
-
-        return arabicNumber;
+        return line.length() == 0 ? arabicNumber : -1;
     }
 
-    // вспомогательный метод
-    // позволяет распарсить следующий один класс римского числа (тысячи, затем сотни и т.д.)
-    // возвращает представление этого класса в виде арабского числа и удаляет
-    // этот класс из StringBuilder line
-    private static int auxiliaryParse(int exp, char one, char five, char ten) {
-        if (line.length() == 0) return -1;
-        String fore = new String("" + one + five);
-        String nine = new String("" + one + ten);
-        int addNumber = 0;
 
-        while (true) {
-            // проверка на 4 и 9 (единиц, десятков или сотней)
-            if (line.length() > 1) {
-                if (line.substring(0,2).equals(fore)) {
-                    addNumber += 4 * exp;
-                    line.delete(0,2);
-                    break;
-                }
-                if (line.substring(0,2).equals(nine)) {
-                    addNumber += 9 * exp;
-                    line.delete(0,2);
-                    break;
-                }
-            }
-            // проверка на 5, 6, 7, 8
-            if (line.charAt(0) == five) {
-                addNumber += 5 * exp;
-                line.deleteCharAt(0);
-                if (line.length() == 0) break;
-                for (int i = 0; i < 4; i++) {
-                    if (line.charAt(0) == one) {
-                        if (i == 3) return -1;
-                        addNumber += 1 * exp;
-                        line.deleteCharAt(0);
-                        if (line.length() == 0) break;
-                    } else break;
-                }
-            } else { // проверка на 1, 2, 3
-                for (int i = 0; i < 4; i++) {
-                    if (line.length() == 0) break;
-                    if (line.charAt(0) == one) {
-                        if (i == 3) return -1;
-                        addNumber += 1 * exp;
-                        line.deleteCharAt(0);
-                        if (line.length() == 0) break;
-                    } else break;
-                }
-            }
-            break;
-        }
-        return addNumber;
+    // определяет является ли крайнее левое число в line, числом,
+    // переданным в этот метод. если да, удаляет его из line
+    private static boolean checkAndPrun(String number) {
+        if (line.length() < number.length()) return false;
+        // проверка
+        int endIndex = number.length();
+        String sub = line.substring(0, endIndex);
+        boolean check = sub.equals(number);
+        // удаление
+        if (check) line.delete(0, endIndex);
+        return check;
     }
 
 }
