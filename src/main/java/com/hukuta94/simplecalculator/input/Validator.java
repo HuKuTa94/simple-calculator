2df6f3ee33c191fdb285c62ac1fcddc76562f377
aveJava <javatalk@mail.ru>
2020-12-09 23:19:37 +0300

    rewritten class Validator

--- src/main/java/com/hukuta94/simplecalculator/input/Validator.java
+++ src/main/java/com/hukuta94/simplecalculator/input/Validator.java
@@ -4,84 +4,24 @@ import com.hukuta94.simplecalculator.model.Data;
 import com.hukuta94.simplecalculator.model.NumberType;
 import com.hukuta94.simplecalculator.model.RomanNumeral;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Validator {
-    private static final int MIN_NUMBER = 1;
-    private static final int MAX_NUMBER = 10;
-    private static final String ROMAN_NUMBERS = "IVXLCM";
-    private static final String REGEX_SPLIT = "\\s";
-    private static final int FIRST_NUMBER_INDEX = 0;
-    private static final int SECOND_NUMBER_INDEX = 2;
-    private static final int OPERATOR_INDEX = 1;
 
     static Data validateLineAndGetData(String line) {
-        String[] strNumbers = line.toUpperCase().split( REGEX_SPLIT );
+        // проверка: ни один из аргументов не должен быть нулем
+        if (line.matches("(^0.*)||(.*\\D0.*)")) throw new IllegalArgumentException("Ex 1");
+        line.concat("df");
 
-        if( strNumbers.length != 3 ) {
-            throw new IllegalArgumentException( "Expected two numbers and one operator ('+', '-', '*', '/')" );
-        }
+        // получение аргументов и операнда из line
+        line.toUpperCase();
+        Matcher roman = Pattern.compile("\\s*([IVXLCDM]+)\\s*([\\+\\-\\*/])\\s*([IVXLCDM]+)\\s*").matcher(line);
+        Matcher arabic = Pattern.compile("\\s*(\\d+)\\s*([\\+\\-\\*/])\\s*(\\d+)\\s*").matcher(line);
+        if (!roman.find() && !arabic.find()) throw new IllegalArgumentException("Ex 1");
+        NumberType type = roman.reset().find() ? NumberType.ROMAN : NumberType.ARABIC;
+        Matcher matcher = type == NumberType.ARABIC ? arabic : roman;  // в этом объекте хранятся аргументы
 
-        // Numbers are not same type
-        NumberType type1 = getNumberType( strNumbers[ FIRST_NUMBER_INDEX ] );
-        NumberType type2 = getNumberType( strNumbers[ SECOND_NUMBER_INDEX ] );
-        if (type1 != type2) {
-            throw new IllegalArgumentException( "Numbers are not same type: " + strNumbers[ FIRST_NUMBER_INDEX ] + ", " + strNumbers[ SECOND_NUMBER_INDEX ]);
-        }
-
-        int[] intNumbers = new int[ 3 ];
-        for( int i = 0; i < strNumbers.length; i++ )
-        {
-            // Parse number
-            if ( i != OPERATOR_INDEX )
-            {
-                intNumbers[i] = parseNumberToInt( strNumbers[i] );
-
-                if( intNumbers[i] < MIN_NUMBER || intNumbers[i] > MAX_NUMBER ) {
-                    throw new IllegalArgumentException( "number " + intNumbers[i] + " is out of range");
-                }
-            }
-            // Validate operator
-            else
-            {
-                if( !validateOperator( strNumbers[ OPERATOR_INDEX ] )) {
-                    throw new IllegalArgumentException( "Unknown arithmetic operator " + strNumbers[ OPERATOR_INDEX ]);
-                }
-            }
-        }
-
-        return new Data(
-                intNumbers[ FIRST_NUMBER_INDEX ],
-                intNumbers[ SECOND_NUMBER_INDEX ],
-                strNumbers[ OPERATOR_INDEX ],
-                getNumberType( strNumbers[ FIRST_NUMBER_INDEX ])
-        );
-    }
-
-    private static int parseNumberToInt( String stringNumber ) {
-        int result = 0;
-
-        // This number is Arabic
-        try {
-            result = Integer.parseInt( stringNumber );
-        }
-        // This number is Roman
-        catch( NumberFormatException e ) {
-            result = RomanNumeral.toArabic( stringNumber );
-        }
-        return result;
-    }
-
-    private static boolean validateOperator( String operator ) {
-        return  operator.equals( "+" ) || operator.equals( "-" ) ||
-                operator.equals( "*" ) || operator.equals( "/" );
-    }
-
-    private static NumberType getNumberType( String number ) {
-        if ( Character.isDigit( number.charAt( 0 )) ) {
-            return NumberType.ARABIC;
-        }
-        else if ( ROMAN_NUMBERS.contains( number.substring( 0, 1 )) ) {
-            return NumberType.ROMAN;
-        }
-        throw new IllegalArgumentException( number + " is not a number" );
+        return new Data(matcher.group(1), matcher.group(3), matcher.group(2), type);
     }
 }
--- src/main/java/com/hukuta94/simplecalculator/model/Data.java
+++ src/main/java/com/hukuta94/simplecalculator/model/Data.java
@@ -1,15 +1,30 @@
 package com.hukuta94.simplecalculator.model;
 
+import com.hukuta94.simplecalculator.parser.RomanToArabicParser;
+
+import java.util.Objects;
+
 public final class Data
 {
-    private int firstNumber;
-    private int secondNumber;
-    private String operator;
-    private NumberType type;
+    private int firstNumber;    // первый операнд
+    private int secondNumber;   // второй операнд
+    private String operator;    // тип операции
+    private NumberType type;    // тип чисел (рим./араб.)
 
     public Data( int firstNumber, int secondNumber, String operator, NumberType type ) {
         this.firstNumber = firstNumber;
         this.secondNumber = secondNumber;
+        this.operator = operator;
+        this.type = type;
+    }
+
+    public Data( String firstNumber, String secondNumber, String operator, NumberType type ) {
+        if (type == NumberType.ROMAN) {
+            firstNumber = String.valueOf(RomanToArabicParser.parse(firstNumber));
+            secondNumber = String.valueOf(RomanToArabicParser.parse(secondNumber));
+        }
+        this.firstNumber = Integer.parseInt(firstNumber);
+        this.secondNumber = Integer.parseInt(secondNumber);
         this.operator = operator;
         this.type = type;
     }
@@ -28,5 +43,21 @@ public final class Data
 
     public NumberType getType() {
         return type;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Data)) return false;
+        Data data = (Data) o;
+        return firstNumber == data.firstNumber &&
+                secondNumber == data.secondNumber &&
+                operator.equals(data.operator) &&
+                type == data.type;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstNumber, secondNumber, operator, type);
     }
 }
