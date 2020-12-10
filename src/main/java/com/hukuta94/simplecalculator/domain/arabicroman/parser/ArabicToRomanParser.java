bfb7e5578573cff4a6911d13f52fb239b7b0c7fb
Nikita Koshelev <39601787+HuKuTa94@users.noreply.github.com>
2020-12-10 22:12:22 +0500

    deleted abstract modificator

--- src/main/java/com/hukuta94/simplecalculator/domain/arabicroman/parser/ArabicToRomanParser.java
+++ src/main/java/com/hukuta94/simplecalculator/domain/arabicroman/parser/ArabicToRomanParser.java
@@ -2,13 +2,14 @@ package com.hukuta94.simplecalculator.domain.arabicroman.parser;
 
 import com.hukuta94.simplecalculator.domain.arabicroman.model.RomanNumeral;
 
-public abstract class ArabicToRomanParser {
-
-    public static String parse( int number ) {
-        String THOUSANDS = RomanNumeral.toRoman((number/1000) * 1000);
-        String HUNDREDS = RomanNumeral.toRoman(((number/100) % 10) * 100);
-        String TENS = RomanNumeral.toRoman(((number/10) % 10) * 10);
-        String UNITS = RomanNumeral.toRoman(number % 10);
+public class ArabicToRomanParser
+{
+    public String parse( int number )
+    {
+        String THOUSANDS = RomanNumeral.toRoman(( number/1000 ) * 1000 );
+        String HUNDREDS  = RomanNumeral.toRoman((( number/100 ) % 10 ) * 100 );
+        String TENS  = RomanNumeral.toRoman((( number/10 ) % 10 ) * 10 );
+        String UNITS = RomanNumeral.toRoman( number % 10 );
 
         return THOUSANDS + HUNDREDS + TENS + UNITS;
     }
