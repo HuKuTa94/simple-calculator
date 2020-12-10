cc1ce275e1cad0d93269d4fa8065e8ee4a35c000
aveJava <javatalk@mail.ru>
2020-12-10 14:20:27 +0300

    some fixes

--- src/main/java/com/hukuta94/simplecalculator/input/Validator.java
+++ src/main/java/com/hukuta94/simplecalculator/input/Validator.java
@@ -11,17 +11,34 @@ public class Validator {
 
     // проверяет корректность введенных в программу данных и возвращает их в виде объекта Data
     static Data validateLineAndGetData(String line) {
+        // создаем Matcher для поиска в line операции и операндов
         Matcher roman = Pattern.compile("\\s*([IVXLCDM]+)\\s*([\\+\\-\\*/])\\s*([IVXLCDM]+)\\s*").matcher(line.toUpperCase());
         Matcher arabic = Pattern.compile("\\s*(\\d+)\\s*([\\+\\-\\*/])\\s*(\\d+)\\s*").matcher(line);
-        boolean isArabic = arabic.find();       // арабские ли числа были введены
-        if (!isArabic && !roman.find()) throw new IllegalArgumentException("Должно быть введено два арабских или римских числа и знак операции");
-        int firstNumber = isArabic ? Integer.parseInt(arabic.group(1)) : RomanToArabicParser.parse(roman.group(1));
-        int secondNumber = isArabic ? Integer.parseInt(arabic.group(3)) : RomanToArabicParser.parse(roman.group(3));
-        NumberType type = isArabic ? NumberType.ARABIC : NumberType.ROMAN;
-        String operator = isArabic ? arabic.group(2) : roman.group(2);
-        if (firstNumber <= 0 || secondNumber <= 0)  throw new IllegalArgumentException("Псевдоримские или отрицательные числа");
+        boolean isArabic = arabic.find();               // арабские ли числа были введены
+        Matcher m = isArabic ? arabic : roman;          // матчер, который будем использовать для поиска в line
+        if (!m.reset().find()) {                        // проверяем, что он что-то нашел
+            throw new IllegalArgumentException("Должно быть введено два арабских или римских числа и знак операции");
+        }
 
-        return new Data(firstNumber, secondNumber, operator , type);
+        // получаем операнды, их тип (араб./рим.) и тип операции (сложение, вычитание ...), введенные пользователем
+        String operand1 = m.group(1);
+        String operation = m.group(2);
+        String operand2 = m.group(3);
+        NumberType type = isArabic ? NumberType.ARABIC : NumberType.ROMAN;
+
+        // формируем объект Data
+        int firstNumber = isArabic ? Integer.parseInt(operand1) : RomanToArabicParser.parse(operand1);
+        int secondNumber = isArabic ? Integer.parseInt(operand2) : RomanToArabicParser.parse(operand2);
+
+        // проверка на допустимость таких операндов
+        if (firstNumber <= 0 || secondNumber <= 0) {
+            throw new IllegalArgumentException("Псевдоримские или отрицательные числа");
+        }
+
+        return new Data(firstNumber, secondNumber, operation , type);
     }
 
 }
+
+
+
