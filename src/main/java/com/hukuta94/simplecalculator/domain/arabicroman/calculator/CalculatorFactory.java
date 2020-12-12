315485cdf5690fcffd20d31154a84b4d18070180
Nikita Koshelev <39601787+HuKuTa94@users.noreply.github.com>
2020-12-12 15:40:06 +0500

    added spaces

--- src/main/java/com/hukuta94/simplecalculator/domain/arabicroman/calculator/CalculatorFactory.java
+++ src/main/java/com/hukuta94/simplecalculator/domain/arabicroman/calculator/CalculatorFactory.java
@@ -5,10 +5,10 @@ import com.hukuta94.simplecalculator.domain.arabicroman.model.NumberType;
 
 public abstract class CalculatorFactory
 {
-    public static Calculator getCalculator(InputData inputData, NumberType type ) {
+    public static Calculator getCalculator( InputData inputData, NumberType type ) {
         return switch( type ) {
-            case ARABIC -> new ArabicCalculator(inputData);
-            case ROMAN -> new RomanCalculator(inputData);
+            case ARABIC -> new ArabicCalculator( inputData );
+            case ROMAN  -> new RomanCalculator( inputData );
         };
     }
 }
